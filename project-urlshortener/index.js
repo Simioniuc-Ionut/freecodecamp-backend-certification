a373ad3454e6df3789d6fe24684165fe1d5f6095
Simioniuc Ionut Mihai <simioniucionut@gmail.com>
2025-07-26 20:08:53 +0300

    Finished

--- project-urlshortener/index.js
+++ project-urlshortener/index.js
@@ -1,59 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 var shortUrlCache = new Map();
+var urlCounter = 1;
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use('/api/shorturl', (req, res, next) => {
-  
-  if (!isValidUrl(req.body.url)) {
-    return res.json({ error: 'invalid url' });
-  }
-  next();
-})
- .post('/api/shorturl', (req, res) => {
-  let url = req.body.url;
-  // Check if URL already exists
-  for (let [key, value] of shortUrlCache.entries()) {
-    if (value === url) {
-      return res.json({ original_url: url, short_url: key });
+app.post(
+  "/api/shorturl",
+  (req, res, next) => {
+    console.log(req.body);
+    if (!isValidUrl(req.body.url)) {
+      console.log("invalid url");
+      return res.json({ error: "invalid url" });
+    } else {
+      next();
     }
-  }
-  let shortUrl = urlCounter++;
-  shortUrlCache.set(shortUrl, url);
-  res.json({ original_url: url, short_url: shortUrl });
-});
-app.get('/api/shorturl/:shorturl', (req, res) => {
+  },
+  (req, res) => {
+    let url = req.body.url;
+    console.log(req.body.url);
+    // Check if URL already exists
+    for (let [key, value] of shortUrlCache.entries()) {
+      if (value === url) {
+        return res.json({ original_url: url, short_url: key });
+      }
+    }
+    // console.log(shortUrlCache);
+    let shortUrl = urlCounter++;
+    shortUrlCache.set(shortUrl, url);
+    res.json({ original_url: url, short_url: shortUrl });
+  },
+);
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
   const originalUrl = shortUrlCache.get(Number(req.params.shorturl));
   if (originalUrl) {
     res.redirect(originalUrl);
   } else {
-    res.json({ error: 'No short URL found for given input' });
+    res.json({ error: "No short URL found for given input" });
   }
-})
-app.listen(port, function() {
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
 
 function isValidUrl(url) {
-  try {
-    new URL(url);
-    return true;
-  } catch (err) {
-    return false;
-  }
+  const urlPattern =
+    /^(https?:\/\/)([\w\-]+\.)+[\w\-]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/i;
+  return urlPattern.test(url);
 }
