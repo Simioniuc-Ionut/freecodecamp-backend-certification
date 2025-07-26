361b4cafbc53717074555fee5a3d0186c7f3578e
Simioniuc Ionut Mihai <simioniucionut@gmail.com>
2025-07-26 18:52:53 +0300

    Finished

---
+++ project-urlshortener/index.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+var shortUrlCache = new Map();
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.use(bodyParser.json());
+
+app.use('/api/shorturl', (req, res, next) => {
+  
+  if (!isValidUrl(req.body.url)) {
+    return res.json({ error: 'invalid url' });
+  }
+  next();
+})
+.post('/api/shorturl', (req, res) => {
+  let url = req.body.url;
+  let shortUrl =  Math.floor(Math.random() * 1000000);
+  let response = {
+    original_url: url,
+    short_url: shortUrl
+  };
+  if (!shortUrlCache.get(shortUrl)) {
+    shortUrlCache.set(shortUrl, url);
+  }
+  res.json(response);
+});
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  const originalUrl = shortUrlCache.get(Number(req.params.shorturl));
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'No short URL found for given input' });
+  }
+})
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
+
+function isValidUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
